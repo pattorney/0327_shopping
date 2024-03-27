34e9ae24f9f47e5b21e40cdef34611cca6e5cc94
pattorney <pcsguies@hanmail.net>
2024-03-27 23:03:35 +0900

    third commit

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -6,7 +6,7 @@ const ProductAll = () => {
   const [productList, setProductList] = useState([]);
 
   const getProducts = async () => {
-    let url = "https://my-json-server.typicode.com/pattorney/0327_shopping/product/${id}";
+    let url = "https://my-json-server.typicode.com/pattorney/0327_shopping/products/${id}";
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
