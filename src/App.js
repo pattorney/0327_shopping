1ce43e9266c235f3d86d19cebc1ba2ff9009a6ac
pattorney <pcsguies@hanmail.net>
2024-03-28 20:45:02 +0900

    ninth commit

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ProductAll />} />
         <Route path="/login" element={<Login setAuthenticate={setAuthenticate} />} />
-        <Route path="/product/:id" element={<PrivateRoute authenticate={authenticate} />} />
+        <Route path="/products/:id" element={<PrivateRoute authenticate={authenticate} />} />
       </Routes>
     </div>
   );
